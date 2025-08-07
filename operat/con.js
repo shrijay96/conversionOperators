9f1bd69620e7a021059462c610ba0c53267c6599
shrijay96 <shrijaykale999@gmail.com>
2025-08-07 09:03:32 +0000

    con.js

---
+++ operat/con.js
@@ -0,0 +1,26 @@
+let score ="shrijay"
+ console.log(typeof score);
+  console.log(typeof (score));
+
+  let valueInnumber =(score)
+  console.log(typeof valueInnumber);
+  console.log(valueInnumber);
+  //"33abc"=> Nan
+  //"33" => 33
+  //true => 1; false=>0;
+
+  let isLogedin =""
+  let bolleanisLogedin = Boolean (isLogedin)
+  console.log(bolleanisLogedin);
+   // 1 =>true; 0=> false;
+   //"" => false
+   // " shrijay" => false
+
+  let somenumber = 44
+  let string = string(somenumber)
+  console.log(somenumber);
+  console.log(typeof somenumber);
+  
+  
+
+ 
